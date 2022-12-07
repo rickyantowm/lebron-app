ba71a52055a81908e96a31cc6f8721a94d3a2f2e
Rickyanto Wangsa Mulya <rickyanto.wangsa@ui.ac.id>
2022-12-08 01:28:06 +0700

    Implement search case insensitive returning name, concat positions and image

--- example_app/urls.py
+++ example_app/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from example_app.views import index
+from example_app.views import index, search_result
 
-app_name = 'example_app'
 
 urlpatterns = [
     path('', index, name='index'),
+    path('search_result', search_result, name='search_result'),
 ]
--- example_app/views.py
+++ example_app/views.py
@@ -1,4 +1,61 @@
 from django.shortcuts import render
+from SPARQLWrapper import SPARQLWrapper, JSON
 
 def index(request):
     return render(request, 'index.html')
+
+
+def search_result(request):
+    response = {}
+    search = request.POST['search']
+    namespace = "kb"
+    sparql = SPARQLWrapper("http://localhost:9999/blazegraph/namespace/"+ namespace + "/sparql")
+
+    sparql.setQuery("""prefix :      <http://127.0.0.1:8000/rdf-data/> 
+prefix owl:   <http://www.w3.org/2002/07/owl#> 
+prefix rdf:   <http://www.w3.org/1999/02/22-rdf-syntax-ns#> 
+prefix rdfs:  <http://www.w3.org/2000/01/rdf-schema#> 
+prefix team:  <http://www.example.org/data/team#> 
+prefix v:     <http://www.example.org/vocab#> 
+prefix vcard: <http://www.w3.org/2006/vcard/ns#>
+prefix xsd:   <http://www.w3.org/2001/XMLSchema#> 
+PREFIX wd: <http://www.wikidata.org/entity/>
+PREFIX wds: <http://www.wikidata.org/entity/statement/>
+PREFIX wdv: <http://www.wikidata.org/value/>
+PREFIX wdt: <http://www.wikidata.org/prop/direct/>
+PREFIX wikibase: <http://wikiba.se/ontology#>
+PREFIX p: <http://www.wikidata.org/prop/>
+PREFIX ps: <http://www.wikidata.org/prop/statement/>
+PREFIX pq: <http://www.wikidata.org/prop/qualifier/>
+PREFIX bd: <http://www.bigdata.com/rdf#>
+
+
+SELECT DISTINCT ?player_name ?country_name ?positions ?image WHERE {
+  SERVICE <https://query.wikidata.org/sparql> {
+    {
+select DISTINCT ?player_name ?country_name ?player_iri ?image (group_concat(distinct ?label_pos;separator=", ") as ?positions)
+where {
+  ?player_iri wdt:P413 ?pos .
+  ?pos rdfs:label ?label_pos .
+  OPTIONAL{?player_iri wdt:P18 ?image .}
+  FILTER (lang(?label_pos) = 'en')
+}GROUP BY ?player_name ?country_name ?player_iri ?image
+    }
+  }
+{SELECT DISTINCT ?player_name ?country_name ?player_iri
+WHERE {
+  ?root rdfs:label ?player_name .
+  ?root :country ?country .
+  ?country rdfs:label ?country_name .
+  ?root :player_iri ?player_iri
+  FILTER contains(LCASE(?player_name), "%s")
+}}
+}""" % search)
+
+    sparql.setReturnFormat(JSON)
+    results = sparql.query().convert()
+    print(results)
+    response['data'] = results["results"]["bindings"]
+
+
+    return render(request, 'search_result.html', response)
