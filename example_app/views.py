0f77bf9852e953dee1241ab27a86f002d144d84a
Rickyanto Wangsa Mulya <rickyanto.wangsa@ui.ac.id>
2022-12-20 22:01:06 +0700

    Insert query

--- example_app/views.py
+++ example_app/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from SPARQLWrapper import SPARQLWrapper, JSON
 
+namespace = "kb"
+sparql = SPARQLWrapper("http://localhost:9999/blazegraph/namespace/"+ namespace + "/sparql")
+
 def index(request):
     return render(request, 'index.html')
 
@@ -8,56 +11,27 @@ def index(request):
 def search_result(request):
     response = {}
     search = request.POST['search']
-    namespace = "kb"
-    sparql = SPARQLWrapper("http://localhost:9999/blazegraph/namespace/"+ namespace + "/sparql")
-
     sparql.setQuery("""prefix :      <http://127.0.0.1:8000/rdf-data/> 
-prefix owl:   <http://www.w3.org/2002/07/owl#> 
 prefix rdf:   <http://www.w3.org/1999/02/22-rdf-syntax-ns#> 
 prefix rdfs:  <http://www.w3.org/2000/01/rdf-schema#> 
-prefix team:  <http://www.example.org/data/team#> 
-prefix v:     <http://www.example.org/vocab#> 
-prefix vcard: <http://www.w3.org/2006/vcard/ns#>
-prefix xsd:   <http://www.w3.org/2001/XMLSchema#> 
-PREFIX wd: <http://www.wikidata.org/entity/>
-PREFIX wds: <http://www.wikidata.org/entity/statement/>
-PREFIX wdv: <http://www.wikidata.org/value/>
-PREFIX wdt: <http://www.wikidata.org/prop/direct/>
-PREFIX wikibase: <http://wikiba.se/ontology#>
-PREFIX p: <http://www.wikidata.org/prop/>
-PREFIX ps: <http://www.wikidata.org/prop/statement/>
-PREFIX pq: <http://www.wikidata.org/prop/qualifier/>
-PREFIX bd: <http://www.bigdata.com/rdf#>
 
-
-SELECT DISTINCT ?player_name ?country_name ?positions ?image WHERE {
-  SERVICE <https://query.wikidata.org/sparql> {
-    {
-select DISTINCT ?player_name ?country_name ?player_iri ?image (group_concat(distinct ?label_pos;separator=", ") as ?positions) ?followers
-where {
-  ?player_iri wdt:P413 ?pos .
-  ?pos rdfs:label ?label_pos .
-  OPTIONAL{?player_iri wdt:P18 ?image .}
-  OPTIONAL{?player_iri wdt:P8687 ?followers .}
-  FILTER (lang(?label_pos) = 'en')
-}GROUP BY ?player_name ?country_name ?player_iri ?image ?followers
-    }
-  }
-{SELECT DISTINCT ?player_name ?country_name ?player_iri
-WHERE {
-  ?root rdfs:label ?player_name .
-  ?root :country ?country .
-  ?country rdfs:label ?country_name .
-  ?root :player_iri ?player_iri
-  FILTER contains(LCASE(?player_name), "%s")
-}}
-}ORDER BY DESC (xsd:integer(?followers))""" % search)
+SELECT DISTINCT ?root ?player_name ?country_name ?player_iri (group_concat(distinct ?team_name;separator=", ") as ?teams)
+WHERE{
+    ?root rdfs:label ?player_name .
+    FILTER contains(LCASE(?player_name),"%s")
+    ?root :season ?seasons .
+	?seasons :hasTeam ?team .
+    ?team rdfs:label ?team_name .
+ 	?root :country ?country .
+    ?country rdfs:label ?country_name .
+  	?root :player_iri ?player_iri
+  	
+    FILTER(strlen(?team_name) > 3)
+}GROUP BY ?root ?player_name ?country_name ?player_iri""" % search)
 
     sparql.setReturnFormat(JSON)
     results = sparql.query().convert()
     response['data'] = results["results"]["bindings"]
-    print(results)
-
     return render(request, 'search_result.html', response)
 
 def get_player_detail(request):
